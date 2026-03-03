0ce2f47108eada321a29c7ff10621569f1c50e5c
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-03-03 11:20:19 +0700

    input

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -5,3 +5,28 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{
+    int d;
+    while (true)
+    {
+        cout << "Masukan banyknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+        {
+            cout << "\nArray dapat mempunyai amaksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "==================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "==================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
